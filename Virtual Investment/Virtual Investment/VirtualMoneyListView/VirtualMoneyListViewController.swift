81fb941717526f0ea4a55404d9df4bd9fe0fd234
조상호 <97chos@naver.com>
2021-02-24 01:38:37 +0900

    VirtualMoneyListViewController 기본 UI 설정 구현

--- Virtual Investment/Virtual Investment/VirtualMoneyListView/VirtualMoneyListViewController.swift
+++ Virtual Investment/Virtual Investment/VirtualMoneyListView/VirtualMoneyListViewController.swift
@@ -11,10 +11,53 @@ import SnapKit
 
 class VirtualMoneyListViewController: UIViewController {
 
+
+  // MARK: UI
+
+  private lazy var searchBar: UISearchBar = {
+    let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 56))
+    searchBar.placeholder = "검색하기"
+    searchBar.searchTextField.backgroundColor = .white
+    searchBar.keyboardType = .asciiCapable
+    return searchBar
+  }()
+  private let tableView: UITableView = {
+    let tableView = UITableView()
+    return tableView
+  }()
+
+
   // MARK: View LifeCycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.configure()
+  }
+
+  override func viewDidLayoutSubviews() {
+    self.searchBar.frame.origin = CGPoint(x: 0, y: self.view.safeAreaInsets.top)
+  }
+
+
+  // MARK: Configuration
+
+  private func configure() {
+    self.viewConfigure()
+    self.layout()
+  }
+
+  private func viewConfigure() {
     self.view.backgroundColor = .white
+    self.title = "거래소"
+  }
+
+  private func layout() {
+    self.view.addSubview(self.tableView)
+    self.view.addSubview(self.searchBar)
+
+    self.tableView.snp.makeConstraints {
+      $0.leading.trailing.bottom.equalToSuperview()
+      $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(self.searchBar.frame.height)
+    }
   }
 }
