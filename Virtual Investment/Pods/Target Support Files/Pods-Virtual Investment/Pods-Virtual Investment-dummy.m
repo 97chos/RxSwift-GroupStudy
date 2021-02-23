f61b4f129fc4685fabccd5f3603256c1c45a6fa5
조상호 <97chos@naver.com>
2021-02-23 23:27:18 +0900

    SnapKit, Socket.IO-Client-Swift 라이브러리 설치

---
+++ Virtual Investment/Pods/Target Support Files/Pods-Virtual Investment/Pods-Virtual Investment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Virtual_Investment : NSObject
+@end
+@implementation PodsDummy_Pods_Virtual_Investment
+@end
---
+++ Virtual Investment/Virtual Investment/Main/MainViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MainViewController.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/02/23.
+//
+
+import Foundation
+import UIKit
+import SocketIO
+import SnapKit
