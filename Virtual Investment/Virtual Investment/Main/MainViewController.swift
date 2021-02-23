8d746917361ad12f75b55b319b1f4d4f9d04176f
조상호 <97chos@naver.com>
2021-02-23 23:41:40 +0900

    UI 요소 추가

--- Virtual Investment/Virtual Investment/Main/MainViewController.swift
+++ Virtual Investment/Virtual Investment/Main/MainViewController.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 import UIKit
-import SocketIO
 import SnapKit
+
+class MainViewController: UIViewController {
+
+  // MARK: UI
+
+  private let mainImageView: UIImageView = {
+    let imgView = UIImageView()
+    return imgView
+  }()
+  private let mainLabel: UILabel = {
+    let label = UILabel()
+    label.text = "사용할 가상 계좌 금액을 입력해주세요."
+    label.font = .systemFont(ofSize: 15)
+    label.sizeToFit()
+    label.textColor = .white
+    return label
+  }()
+  private let textField: UITextField = {
+    let textField = UITextField()
+    textField.keyboardType = .numberPad
+    return textField
+  }()
+  private let nextButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("다음", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    return button
+  }()
+
+}
