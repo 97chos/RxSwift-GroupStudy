90b15217290735b09554fa9854525a5412cbf8fa
조상호 <97chos@naver.com>
2021-02-24 00:24:01 +0900

    가상 화페 리스트를 보여줄 VirtualMoneyListViewController 추가

---
+++ Virtual Investment/Virtual Investment/VirtualMoneyListView/VirtualMoneyListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VirtualMoneyListViewController.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/02/24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class VirtualMoneyListViewController: UIViewController {
+
+  // MARK: View LifeCycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
