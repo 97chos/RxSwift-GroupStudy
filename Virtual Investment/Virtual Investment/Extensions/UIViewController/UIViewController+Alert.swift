103e9dce1fcbfaae712e9ede197998e99fbda391
조상호 <97chos@naver.com>
2021-02-27 00:06:47 +0900

    UIViewController Extension으로 alert 추가

---
+++ Virtual Investment/Virtual Investment/Extensions/UIViewController/UIViewController+Alert.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Alert.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/02/26.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+
+  func alert(title: String?, message: String?, completion: (() -> Void)? ) {
+    DispatchQueue.main.async {
+      let alert: UIAlertController = {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        return alert
+      }()
+
+      alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in completion?() })
+
+      self.present(alert, animated: true)
+    }
+  }
+}
