35e7a590e80f46c705d43b76cf6d3cd724b7029a
조상호 <97chos@naver.com>
2021-02-26 23:25:18 +0900

    앱 첫 실행 후 실시간 데이터를 불러오기 전, 셀에 할당할 시세 스냅샷을 불러오는 API 구현

--- Virtual Investment/Virtual Investment/APIService/APIService.swift
+++ Virtual Investment/Virtual Investment/APIService/APIService.swift
@@ -8,6 +8,10 @@
 import Foundation
 import Alamofire
 
+enum APIError: Error {
+  case parseError
+}
+
 class APIService {
 
   // MARK: Lookup Virtual List
@@ -28,6 +32,36 @@ class APIService {
       }
     } catch {
       return []
+    }
+  }
+
+
+  // MARK: Load Initializing Data
+
+  func loadCoinsData(codes: [String], completion: @escaping (Result<[ticker],Error>) -> Void) {
+
+    let codeList = codes.joined(separator: ",")
+    var priceListData: [ticker] = []
+
+    let param: Parameters = ["markets" : codeList]
+    guard let url: URL = URL(string: "https://api.upbit.com/v1/ticker") else { return }
+
+    AF.request(url, method: .get, parameters: param, encoding: URLEncoding.queryString).responseJSON { response in
+      do {
+        guard let result = try response.result.get() as? [[String:Any]] else { return }
+
+        result.forEach {
+          guard let currentPrice = $0["trade_price"] as? Double else { return }
+          guard let code = $0["market"] as? String else { return }
+          guard let highPrice = $0["high_price"] as? Double else { return }
+          guard let lowPrice = $0["low_price"] as? Double else { return }
+          let coinData = ticker(currentPrice: currentPrice, code: code, highPrice: highPrice, lowPrice: lowPrice)
+          priceListData.append(coinData)
+          completion(.success(priceListData))
+        }
+      } catch {
+        print(error.localizedDescription)
+      }
     }
   }
 }
---
+++ Virtual Investment/Virtual Investment/VirtualMoneyListView/Model/CoinsTickerDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsTickerDataModel.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/02/26.
+//
+
+import Foundation
