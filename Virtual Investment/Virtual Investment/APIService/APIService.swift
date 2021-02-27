624c67f7d82bf7a823bfbbd41e3830e0059a0b0b
조상호 <97chos@naver.com>
2021-02-27 17:02:51 +0900

    코드 가독성 정리 및 누락된 설정 파일 추가

--- Virtual Investment/Virtual Investment/APIService/APIService.swift
+++ Virtual Investment/Virtual Investment/APIService/APIService.swift
@@ -46,13 +46,15 @@ class APIService {
     var priceListData: [ticker] = []
 
     let param: Parameters = ["markets" : codeList]
+
     guard let url: URL = URL(string: "https://api.upbit.com/v1/ticker") else { completion(.failure(APIError.urlError))
       return
     }
 
     AF.request(url, method: .get, parameters: param, encoding: URLEncoding.queryString).responseJSON { response in
       do {
-        guard let result = try response.result.get() as? [[String:Any]] else { completion(.failure(APIError.parseError))
+        guard let result = try response.result.get() as? [[String:Any]] else {
+          completion(.failure(APIError.parseError))
           return
         }
 
