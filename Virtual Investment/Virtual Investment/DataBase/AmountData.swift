61992c449693011a60c854d4693331faddcf13e2
조상호 <97chos@naver.com>
2021-03-02 18:06:00 +0900

    AmountData 및 mainViewController Reative 방식으로 변경 구현

--- Virtual Investment/Virtual Investment/DataBase/AmountData.swift
+++ Virtual Investment/Virtual Investment/DataBase/AmountData.swift
@@ -6,25 +6,35 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class AmountData {
   static var shared = AmountData()
-  var deposit: Double = 0
-  var investedPrice: Double{
-    get {
-      return boughtCoins.reduce(0){ $0 + $1.totalBoughtPrice}
-    }
-  }
-  var boughtCoins: [Coin] = []
+  var deposit: BehaviorSubject = BehaviorSubject<Double>(value: 0)
+  var boughtCoins: BehaviorRelay<[Coin]> = BehaviorRelay<[Coin]>(value: [])
 
-  func getEvaluatedPrice() -> Double {
-    var price: Double = 0
-    boughtCoins.forEach {
-      var coin = $0
-      price += Double(coin.holdingCount) * (coin.prices?.currentPrice ?? 0)
+  lazy var investedPrice: Observable<Double> = boughtCoins
+    .map{ $0.reduce(0){$0 + $1.totalBoughtPrice}}
+
+  lazy var evaluatedPrice: Observable<Double> = boughtCoins
+    .map{
+      var price: Double = 0
+      $0.forEach{
+        var coin = $0
+        price += Double(coin.holdingCount) * (coin.prices?.currentPrice ?? 0)
+      }
+      return price
     }
-    return price
-  }
+
+//  func getEvaluatedPrice() -> Double {
+//    var price: Double = 0
+//    boughtCoins.forEach {
+//      var coin = $0
+//      price += Double(coin.holdingCount) * (coin.prices?.currentPrice ?? 0)
+//    }
+//    return price
+//  }
 
   private init() {
   }
