6d4768f2f2edf121a624595b1ac2c4463c1c63fa
조상호 <97chos@naver.com>
2021-03-22 12:19:55 +0900

    UserDefaults 사용을 위한 Key 생성

---
+++ Virtual Investment/Virtual Investment/Utilities/Enum/UserDefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKeys.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/03/22.
+//
+
+import Foundation
+
+enum UserDefaultsKey {
+  static let isExistingUser = "isExistingUser"
+  static let remainingDeposit = "remainingDeposit"
+}
