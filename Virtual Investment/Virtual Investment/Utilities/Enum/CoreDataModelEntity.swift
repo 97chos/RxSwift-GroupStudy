fe35710de28aaaa45d09ab4778fa9f00c0c7caae
조상호 <97chos@naver.com>
2021-03-22 16:20:51 +0900

    CoreDataEntity 전용 enum 생성

---
+++ Virtual Investment/Virtual Investment/Utilities/Enum/CoreDataModelEntity.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataModelEntity.swift
+//  Virtual Investment
+//
+//  Created by sangho Cho on 2021/03/22.
+//
+
+import Foundation
+
+enum CoreDataModelEntity {
+  static let coinInfo = "CoinInfo"
+  static let ticker = "Ticker"
+}
