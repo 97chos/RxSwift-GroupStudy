2a8867c51b168d250edc3b2a9fb90d31805f8919
조상호 <97chos@naver.com>
2021-03-29 00:00:41 +0900

    WebSocketDelegate 생성

--- Virtual Investment/Virtual Investment/Utilities/RxStarscream/RxWebSocketDelegateProxy.swift
+++ Virtual Investment/Virtual Investment/Utilities/RxStarscream/RxWebSocketDelegateProxy.swift
@@ -5,4 +5,52 @@
 //  Created by sangho Cho on 2021/03/28.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import Starscream
+
+public class RxWebSocketDelegateProxy: DelegateProxy<WebSocket, WebSocketDelegate>, DelegateProxyType, WebSocketDelegate {
+
+  // MARK: Properties
+
+  fileprivate let didReceiveSubject = PublishSubject<WebSocketEvent>()
+
+
+  // MARK: Initialzing
+
+  deinit {
+    self.didReceiveSubject.onCompleted()
+  }
+
+
+  // MARK: DelegateProxyType
+
+  public static func registerKnownImplementations() {
+    self.register { (socket) -> RxWebSocketDelegateProxy in
+      RxWebSocketDelegateProxy(parentObject: socket, delegateProxy: self)
+    }
+  }
+
+  public static func currentDelegate(for object: WebSocket) -> WebSocketDelegate? {
+    object.delegate
+  }
+
+  public static func setCurrentDelegate(_ delegate: WebSocketDelegate?, to object: WebSocket) {
+    object.delegate = delegate
+  }
+
+
+  // MARK: WebSocketDelegate
+
+  public func didReceive(event: WebSocketEvent, client: WebSocket) {
+    self.didReceiveSubject.onNext(event)
+  }
+}
+
+extension WebSocket: ReactiveCompatible { }
+extension Reactive where Base: WebSocket {
+  public var didReceive: ControlEvent<WebSocketEvent> {
+    let soruce = RxWebSocketDelegateProxy.proxy(for: self.base).didReceiveSubject
+    return ControlEvent(events: soruce)
+  }
+}
